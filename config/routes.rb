de9f5d4b1ce3bfdd72cc584ee541ab7022ce2bfc
Talâa Zina <34269322+zinagithub@users.noreply.github.com>
2019-04-12 15:15:20 +0100

    Revert "Dev"

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  get 'login', to: 'sessions#new', as: "login"
-  get 'logout', to: 'sessions#destroy', as: "logout"
-  get 'signup', to: 'users#new', as: "signup"
-  resources :users
-  resources :sessions
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
