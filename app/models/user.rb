11efe37d3f76d18ea8ee07a8b951e5185fee6cfc
YellowPipe <philippdmitrienko@gmail.com>
2019-04-12 17:01:55 +0300

    login/logout functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  before_create :remember
+
   before_save { self.email = email.downcase }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
@@ -7,4 +9,18 @@ class User < ApplicationRecord
   
   validates :password, presence: true, length: { minimum: 6 }
   has_secure_password
+
+
+  def User.new_token
+  	SecureRandom.urlsafe_base64
+  end
+
+  def User.digest(token)
+		Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  def remember
+  	self.remember_token = User.digest(User.new_token)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-
+  get 'login', to: 'sessions#new', as: "login"
+  get 'logout', to: 'sessions#destroy', as: "logout"
   get 'signup', to: 'users#new', as: "signup"
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :sessions
 end
